b6c698e9c30b9970b1d235af7d29e666de586d67
okunishinishi <okunishinishi@gmail.com>
2018-06-16 16:22:37 +0900

    Releasing new package

--- lib/TheMainStyle.jsx
+++ lib/TheMainStyle.jsx
@@ -4,7 +4,7 @@ import classnames from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { asStyleData } from 'the-component-util'
-import TheStyle from 'the-style'
+import { TheStyle } from 'the-style'
 
 /** Style for TheMain */
 const TheMainStyle = ({className, id, options}) => (
